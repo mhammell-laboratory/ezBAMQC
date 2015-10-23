f519e9d20e6a6a740460231eccb9aa31125d6b0d
Oliver Tam <tam@cshl.edu>
2015-10-23 18:27:35 -0400

    setup.py: removed gitlab/master line; changed RSeQC to BAMQC; changed website from BAMQC to BAMqc

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with the distribution).
 """
 
 if sys.version_info[0] != 2 or sys.version_info[1] < 7:
-	print >> sys.stderr, "ERROR: RSeQC requires Python 2.7"
+	print >> sys.stderr, "ERROR: BAMQC requires Python 2.7"
 	sys.exit()
 
 from setuptools import setup, Extension
@@ -21,7 +21,6 @@ from Cython.Build import cythonize
 #from distutils.core import Command
 # Use build_ext from Cython
 
->>>>>>> gitlab/master
 command_classes = {}
 
 # Use build_ext from Cython if found
@@ -48,7 +47,7 @@ setup(name = "BAMQC",
       author_email ="yjin@cshl.edu",
       license='GPLv3',
       platforms = ['Linux','MacOS'],
-      url='http://hammelllab.labsites.cshl.edu/software#BAMQC',
+      url='http://hammelllab.labsites.cshl.edu/software#BAMqc',
       long_description=readme(),
       classifiers=[
             'Development Status :: 4 - Beta',
